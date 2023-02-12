082134c9402f157a2292da2104852ff0e1d79e7b
Pavlo Abolmasov <abolmasovp@gmail.com>
2023-02-12 13:10:48 +0200

    Added Modules v.1. Subnet + Webserver

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
 output "ec2_public_ips" {
-    value = aws_instance.development.public_ip
-}
-
-output "aws_ami_id" {
-    value = data.aws_ami.latest-amazon-linux-image.id
+    value = module.my-webserver.instance.public_ip
 }
